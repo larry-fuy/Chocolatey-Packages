b76b5c4a18f17a612bf8f8732ab8212a1049215b
larry-fuy <larry.yong.fu@gmail.com>
2014-09-17 11:46:46 -0500

    simplify powershell script

--- vcexpress2010/Tools/ChocolateyInstall.ps1
+++ vcexpress2010/Tools/ChocolateyInstall.ps1
@@ -1,7 +1,1 @@
-if(Get-Module Boxstarter.Chocolatey){
-    if(Test-PendingReboot){
-        Invoke-Reboot
-    }
-}
-
 Install-ChocolateyPackage 'vcexpress2010' 'exe' "/Passive /NoRestart /Log $env:temp\vcexpress2010.log" 'http://go.microsoft.com/?linkid=9709949'
